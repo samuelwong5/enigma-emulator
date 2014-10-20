4f20d60eed8ce4904e79de1b719f27f5470f6c3d
Samuel Wong <sw5713@ic.ac.uk>
2014-10-20 17:25:44 +0100

    Fixed whitespace

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+bool isWhiteSpace(char c);
+
 int main(int argc, char **argv)
 {
 	Encryptor *encryptor = new Encryptor(argc, argv);                                                  
@@ -15,7 +17,15 @@ int main(int argc, char **argv)
         for(char& c : line){
             int input = (int) c - 'A';
             if (input < 0 || input > 25){ exit(1); }                                         // Exit if not A-Z
-            encryptor->encryptChar(input);        
+			if (!isWhiteSpace(c)){ encryptor->encryptChar(input); }
         }
     }
+}
+
+bool isWhiteSpace(char c)
+{
+	if (c == ' ' || c == '\r' || c == "\n" || c == "\t" || c == "\f"){
+		return true;
+	} 
+	return false;
 }
